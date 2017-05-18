fe16d540939184322b1981a0b623b0eae53d7f6f
Alfio Emanuele <alfio.emanuele.f@gmail.com>
2017-05-18 17:15:01 +0100

    Keyboard controller (control the vehicle by using the WASD keys)

---
+++ TrafficPrototype/Assets/KeyboardVehicleAgentController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/**
+ * A simple script to control a VehicleAgent using the WASD keys.
+ */
+[RequireComponent (typeof (VehicleAgent))]
+public class KeyboardVehicleAgentController : MonoBehaviour {
+
+	public KeyCode forwardKey = KeyCode.W;
+	public KeyCode backwardKey = KeyCode.S;
+	public KeyCode leftKey = KeyCode.A;
+	public KeyCode rightKey = KeyCode.D;
+
+	// Update is called once per frame
+	void Update () {
+
+		VehicleAgent vehicle = gameObject.GetComponent<VehicleAgent> ();
+
+		if (Input.GetKey (forwardKey))
+			vehicle.Accelerate ();
+
+		if (Input.GetKey (backwardKey))
+			vehicle.Brake ();
+
+		if (Input.GetKey (leftKey))
+			vehicle.SteerLeft ();
+
+		if (Input.GetKey (rightKey))
+			vehicle.SteerRight ();
+		
+	}
+
+}
