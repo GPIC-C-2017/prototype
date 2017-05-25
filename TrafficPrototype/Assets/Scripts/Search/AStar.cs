7313e274f25446bb693dc76027d783b3de450ae9
Paulius Imbrasas <cremboc@gmail.com>
2017-05-25 12:13:34 +0100

    Slightly better path finding algorithm Now includes first waypoint

--- TrafficPrototype/Assets/Scripts/Search/AStar.cs
+++ TrafficPrototype/Assets/Scripts/Search/AStar.cs
@@ -25,13 +25,13 @@ namespace Search {
 
         private List<Waypoint> FindPath() {
             var path = new List<Waypoint>();
-            var success = Search(startNode);
-            if (success) {
+            if (Search(startNode)) {
                 var node = endNode;
                 while (node.ParentNode != null) {
                     path.Add(node.Waypoint);
                     node = node.ParentNode;
                 }
+                path.Add(startNode.Waypoint);
                 path.Reverse();
             }
             return path;
@@ -42,9 +42,9 @@ namespace Search {
             List<Node> nextNodes = GetAdjacentWalkableNodes(currentNode);
             nextNodes.Sort((node1, node2) => node1.F.CompareTo(node2.F));
             foreach (var nextNode in nextNodes) {
-                if (nextNode.Waypoint == endNode.Waypoint) {
+                if (nextNode.Waypoint == endNode.Waypoint)
                     return true;
-                }
+                
                 if (Search(nextNode)) // Note: Recurses back into Search(Node)
                     return true;
             }
@@ -56,7 +56,6 @@ namespace Search {
             var nextLocations = adjacentLocations(fromNode.Waypoint);
 
             foreach (var location in nextLocations) {
-
                 Node node;
                 if (!nodes.TryGetValue(location, out node)) {
                     node = new Node(location, endNode.Waypoint) {ParentNode = fromNode};
@@ -67,7 +66,7 @@ namespace Search {
                 switch (node.State) {
                     case NodeState.Closed:
                         continue;
-                        
+
                     // Already-open nodes are only added to the list if their G-value is lower going via this route.
                     case NodeState.Open:
                         var traversalCost = Node.GetTraversalCost(node.Location, node.ParentNode.Location);
@@ -77,19 +76,17 @@ namespace Search {
                             walkableNodes.Add(node);
                         }
                         break;
-                        
+
                     case NodeState.Untested:
                         // If it's untested, set the parent and flag it as 'Open' for consideration
                         node.ParentNode = fromNode;
                         node.State = NodeState.Open;
                         walkableNodes.Add(node);
                         break;
-                        
+
                     default:
                         throw new ArgumentOutOfRangeException();
                 }
-
-                
             }
 
             return walkableNodes;
@@ -123,7 +120,7 @@ namespace Search {
                 G = parentNode.G + GetTraversalCost(Location, parentNode.Location);
             }
         }
-        
+
         public Node(Waypoint waypoint, Waypoint endWaypoint) {
             Waypoint = waypoint;
             Location = waypoint.transform.position;
@@ -133,9 +130,7 @@ namespace Search {
         }
 
         internal static float GetTraversalCost(Vector3 location, Vector3 otherLocation) {
-            var deltaX = otherLocation.x - location.x;
-            var deltaY = otherLocation.y - location.y;
-            return Mathf.Sqrt(deltaX * deltaX + deltaY * deltaY);
+            return (location - otherLocation).sqrMagnitude;
         }
     }
 
