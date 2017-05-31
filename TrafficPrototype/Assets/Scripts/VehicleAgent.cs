edcfd04389130f2c22e5175b48950cde49a330b3
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2017-05-31 10:59:02 +0100

    Vehicle Agent now steers sharply (not using the Physics engine)

--- TrafficPrototype/Assets/Scripts/VehicleAgent.cs
+++ TrafficPrototype/Assets/Scripts/VehicleAgent.cs
@@ -14,24 +14,24 @@ public class VehicleAgent : MonoBehaviour {
 	// Public variables
 
 	[Range (10.0f, 100.0f)]
-	public float maximumVehicleSpeed = 85.0f;
+	public float maximumVehicleSpeed = 40.0f;
 
 	[Range (0.05f, 1.0f)]
 	public float steeringResponsiveness = 0.65f;
 
-    [Range(10.0f, 1000.0f)]
-    public float maxSteeringTorque = 800.0f;
+    [Range(10.0f, 500.0f)]
+    public float maxSteeringTorque = 500.0f;
 
     [Range(0.5f, 500.0f)]
-    public float minSteeringTorque = 10.0f;
+    public float minSteeringTorque = 5.0f;
 
     [Range (700.0f, 1100.0f)]
 	public float minimumVehicleTorque = 750.0f;
 
 	[Range (500.0f, 5000.0f)]
-	public float maximumVehicleTorque = 1100.0f;
+	public float maximumVehicleTorque = 700.0f;
 
-	public float lateralForceToTorqueRatio = 0.25f;
+	public float lateralForceToTorqueRatio = 0.00f;
 
 
 	// Public Methods 
@@ -78,9 +78,17 @@ public class VehicleAgent : MonoBehaviour {
 	private Rigidbody rb;
 
 	public void Steer(Vector3 direction) {
-		
-		// TORQUE is a rotation force across the Y axis. Clockwise is positive (up), anticlockwise is negative (-up).
+
 		Vector3 torqueDirection = direction == Vector3.right ? Vector3.up : Vector3.down; 
+		Vector3 rotation = torqueDirection * GetVehicleSteeringTorque () * Time.fixedDeltaTime;
+		gameObject.transform.Rotate (rotation);
+
+		// Rotate the momentum vector
+		rb.velocity = gameObject.transform.forward * rb.velocity.magnitude;
+
+		return;
+
+		// TORQUE is a rotation force across the Y axis. Clockwise is positive (up), anticlockwise is negative (-up).
 		Vector3 torqueVector = torqueDirection * GetVehicleSteeringTorque () * Time.fixedDeltaTime;
 		rb.AddRelativeTorque (torqueVector);
 
